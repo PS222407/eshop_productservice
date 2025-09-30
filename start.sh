44f2bc701c36d7072cd35ad9866092ffb47462c0
jens <a@a.A>
2025-09-30 09:47:10 +0200

    shell scripts

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker compose up -d
+cd ..
+gnome-terminal --title="ProductService" -- bash -c "cd eshop_productservice/eshop_productservice && dotnet run; exec bash"
+gnome-terminal --title="UserService" -- bash -c "cd eshop_userservice/eshop_userservice && dotnet run; exec bash"
+gnome-terminal --title="CartService" -- bash -c "cd eshop_cartservice/eshop_cartservice && dotnet run; exec bash"
