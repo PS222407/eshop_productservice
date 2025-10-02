b6415651813db4c830cb069118bc60bde01d21fb
jens <a@a.A>
2025-10-02 18:45:26 +0200

    added searchengine

--- eshop_productservice/Services/ProductService.cs
+++ eshop_productservice/Services/ProductService.cs
@@ -2,6 +2,7 @@ using eshop_productservice.DTOs;
 using eshop_productservice.Interfaces;
 using eshop_productservice.Models;
 using eshop_productservice.Requests;
+using Typesense.Setup;
 
 namespace eshop_productservice.Services;
 
@@ -19,6 +20,17 @@ public class ProductService(IProductRepository repository) : IProductService
 
     public async Task<PaginationDto<Product>> SearchAsync(SearchRequest searchRequest)
     {
+        var provider = new ServiceCollection()
+            .AddTypesenseClient(config =>
+            {
+                config.ApiKey = "mysecretapikey";
+                config.Nodes = new List<Node>
+                {
+                    new Node("0d6auj5nts1ri4xgp-1.a1.typesense.net", "443", "https")
+                };
+            }, enableHttpCompression: false).BuildServiceProvider();
+        
+        
         return await repository.SearchAsync(searchRequest);
     }
 }
