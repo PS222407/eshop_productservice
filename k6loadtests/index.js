edce6b5bb52c90419982c65d30f47201801342e5
jens <a@a.a>
2025-10-28 20:35:33 +0100

    load tests

--- k6loadtests/index.js
+++ k6loadtests/index.js
@@ -11,17 +11,23 @@ export const options = {
     ],
 };
 
-const BASE_URL = 'http://0.0.0.0:5077/api/productservice';
+const BASE_URL = 'http://192.168.49.2'; // k8s
+// const BASE_URL = 'http://0.0.0.0:5077'; // docker compose
 
 export default function (data) {
-    let res = http.get(url, {
+    let res = http.get(BASE_URL + '/api/productservice/v1/Category', {
         headers: { 'Content-Type': 'application/json' }
-    });
-
-    check(res, {
-        'status is 200': (r) => r.status === 200,
-        'response not empty': (r) => r.body.length > 0,
     });
 
     sleep(1);
 }
+
+// docker compose
+// avg=136.82ms min=316.53µs med=11.83ms max=1.07s p(90)=604.66ms p(95)=792.38ms
+
+// k8s 1st run (pods are busy upsizing)
+// avg=4.56s min=40.47ms med=3.69s max=9.81s  p(90)=8.56s p(95)=9.11s
+// avg=3.81s min=40.17ms med=3.04s max=8.5s   p(90)=6.89s p(95)=7.37s
+// k8s 2nd run (when pods are already upscaled by 1st run)
+// avg=48.73ms min=40.44ms med=47.61ms max=79.95ms p(90)=54.63ms p(95)=57.8ms
+// avg=49.49ms min=40.63ms med=48.46ms max=83.18ms p(90)=55.61ms p(95)=58.7ms
